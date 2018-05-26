c2a600a4d77f5fc419a13b0712a2dadd6930c095
Vinicius Pacheco <vfpweb@gmail.com>
2018-05-26 00:52:45 -0300

    Update redis config

--- vendor/github.com/viniciusfeitosa/matrix_mutante/main.go
+++ vendor/github.com/viniciusfeitosa/matrix_mutante/main.go
@@ -13,10 +13,10 @@ func main() {
 	flag.StringVar(&db.Address, "redis_address", os.Getenv("APP_RD_ADDRESS"), "Redis Address")
 	flag.StringVar(&db.Auth, "redis_auth", os.Getenv("APP_RD_AUTH"), "Redis Auth")
 	flag.StringVar(&db.DB, "redis_db_name", os.Getenv("APP_RD_DBNAME"), "Redis DB name")
-	flag.IntVar(&db.MaxIdle, "redis_max_idle", 100, "Redis Max Idle")
-	flag.IntVar(&db.MaxActive, "redis_max_active", 100, "Redis Max Active")
+	flag.IntVar(&db.MaxIdle, "redis_max_idle", 10, "Redis Max Idle")
+	flag.IntVar(&db.MaxActive, "redis_max_active", 10, "Redis Max Active")
 	flag.IntVar(&db.IdleTimeoutSecs, "redis_timeout", 60, "Redis timeout in seconds")
-	flag.IntVar(&numWorkers, "num_workers", 10, "Number of workers to consume queue")
+	flag.IntVar(&numWorkers, "num_workers", 1, "Number of workers to consume queue")
 	flag.Parse()
 
 	db.Pool = db.NewDBPool()
